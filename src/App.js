6272f5eaae8b7e328a5a7a358b4ddd7f1931687c
chiar015 <chiar015@d.umn.edu>
2024-08-19 11:27:02 -0500

    added home and navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import './App.css';
+import "./App.css";
+import NavBar from "./NavBar";
+import Home from "./Home";
 
 function App() {
   const title = "Welcome to the new module";
   const likes = 50;
   const link = "http://www.google.com";
 
-  const person = {name: 'name', age: 30};
+  const person = { name: "name", age: 30 };
   return (
     <div className="App">
+      <NavBar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked  {likes} times</p>
-        <p>{10}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{Math.random() * 10}</p>
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
     </div>
   );
