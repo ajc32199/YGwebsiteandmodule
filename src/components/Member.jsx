7003ed5608ac535c52ffb34d73f0636768fe7617
chiar015 <chiar015@d.umn.edu>
2024-08-22 18:09:39 -0500

    added logo and working on information fill in page

--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -109,7 +109,31 @@ export default function Member() {
               you share/enter.
             </p>
           </div>
-          <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8"></div>
+          <p className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8"></p>
+        </div>
+
+        <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8">
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-slate-900"
+            >
+              Name
+            </label>
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="Enter full name"
+                  value={form.name}
+                  onChange={(e) => updateForm({ name: e.target.value })}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </form>
     </>
